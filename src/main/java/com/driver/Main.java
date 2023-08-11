39ee8b1ceff2838e776e183d9ebc89eb294e6479
vshubham331998 <vshubham331998@gmail.com>
2023-08-11 20:21:42 +0530

    assignment submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        B objB = new B();
+
+        System.out.println(objB.meth());
+    }
 }
